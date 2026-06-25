8219f86704ef9215b7de99da007416fb8ec1c654
arpitsharma2028 <arpitsharma6105@gmail.com>
2026-06-25 17:14:08 +0530

    Color changing fun using callback hell and Promises

--- JS/Basic Javascript/lect8 = Promise/promise.js
+++ JS/Basic Javascript/lect8 = Promise/promise.js
@@ -66,3 +66,53 @@ saveToDB("apna college")
     .catch((error) => {
         console.log("promise was rejected , with error " , error);
     });
+
+
+// Callback Hell
+let h1 = document.querySelector("h1");
+
+// function changeColor(color, delay, nextColorChange) {
+//     setTimeout(() => {
+//         h1.style.color = color;
+//         if (nextColorChange) nextColorChange();
+//     }, delay);
+// }
+
+// changeColor("red", 1000, () => {
+//     changeColor("orange", 1000, () => {
+//         changeColor("green", 1000, () => {
+//             changeColor("yellow", 1000, () => {
+//                 changeColor("blue", 1000);
+//             });
+//         });
+//     });
+// });
+
+// Using Promises
+
+
+function changeColor(color, delay) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            h1.style.color = color;
+            resolve("color changed!");
+        }, delay);
+    });
+}
+
+changeColor("red", 1000)
+    .then(() => {
+        console.log("red color was completed");
+        return changeColor("orange", 1000);
+    })
+    .then(() => {
+        console.log("orange color was completed");
+        return changeColor("green", 1000);
+    })
+    .then(() => {
+        console.log("green color was completed");
+        return changeColor("blue", 1000);
+    })
+    .then(() => {
+        console.log("blue color was completed");
+    });
